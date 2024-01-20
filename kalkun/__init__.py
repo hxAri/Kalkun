78a98c7a2acf03a043c8d077860a014f2e08e277
hxAri <hxari@proton.me>
2024-01-20 21:53:42 +0700

    Initial commit

--- kalkun/__init__.py
+++ kalkun/__init__.py
@@ -1,2 +1,10 @@
 
+from kalkun.blibli import *
+from kalkun.bukalapak import *
+from kalkun.elevenia import *
+from kalkun.lazada import *
+from kalkun.orami import *
+from kalkun.shopee import *
+from kalkun.tokopedia import *
+from kalkun.zalora import *
 
